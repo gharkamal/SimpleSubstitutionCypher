00a5123c30448f3536bb3af07f1875bdfcff782e
gharkamal <Grewal@Gs-MacBook-Pro.local>
2017-02-19 15:02:30 -0800

    add frequency count and ask user to guess key

--- SimpleSubstitution.java
+++ SimpleSubstitution.java
@@ -21,12 +21,19 @@ public class SimpleSubsitution {
         return countArray;
         
     }
+    public static String[] swapText(String[] key)
+    {
+        
+        return key;
+        
+    }
     
     public static void main(String[] args)
     {
-        
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter CypherText: ");
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String[] key = new String[26];
+        //System.out.println("Enter CypherText: ");
         int[] count = frequency("GBSXUCGSZQGKGSQPKQKGLSKASPCGBGBKGUKGCEUKUZKGGBSQEICA"
                                 + "CGKGCEUERWKLKUPKQQGCIICUAEUVSHqKGCEUPCGBCGQOEVSHUNSU"
                                 + "GKUZCGQSNLSHEHIEEDCUOGEPKHZGBSNKCUGSUKUASERLSKASCUGB"
@@ -36,9 +43,23 @@ public class SimpleSubsitution {
                                 + "EHNKIHSLIMGEKHSASUGKNSHCAKUNSQQKOSPBCISGBCqHSLIMQGKG"
                                 + "SZGBKGCGQSSNSZXQSISQQGEAEUGCUXSGBSSJCqGCUOZCLIENKGCA"
                                 + "USOEGCKGCEUqCGAEUGKCUSZUEGBHSKGEHBCUGERPKHEHKHNSZKGGKAD");
-        for(int a: count)
+        for(int i = 0; i < count.length; i++)
         {
-            System.out.println(a);
+            System.out.println(alphabet.charAt(i) + " :" + count[i]);
+        }
+        
+        
+        
+        System.out.println("Guess key: ");
+        for(int i = 0;i < 26; i++)
+        {
+            System.out.println(alphabet.charAt(i) + ": ");
+            key[i] = input.nextLine();
+        }
+        for(int i = 0; i < key.length; i++)
+        {
+            System.out.println(alphabet.charAt(i) + " :" + key[i]);
         }
     }
+    
 }
